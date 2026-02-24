efdf51df5be09c4ae863549b9efad70ea319a314
Harriet Effah <harriet.effah@amalitech.com>
2026-02-24 14:14:36 +0000

    feat: Implemented BasePage abstract class with common functionality for all pages

--- src/main/java/com/xyz/pages/BasePage.java
+++ src/main/java/com/xyz/pages/BasePage.java
@@ -1,4 +1,47 @@
 package com.xyz.pages;
 
-public class BasePage {
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.xyz.models.TestData;
+
+import java.time.Duration;
+
+//All page objects should extend this class
+//Base page provides common functionality and driver management
+
+public abstract class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected WebDriverWait longWait;
+
+    //Constructor initializes WebDriver, waits, and PageFactory elements
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        // Initialize explicit waits with different timeouts for flexibility
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(TestData.DEFAULT_TIMEOUT_SECONDS));
+        this.longWait = new WebDriverWait(driver, Duration.ofSeconds(TestData.EXTENDED_TIMEOUT_SECONDS));
+
+        // Initialize PageFactory elements - enables @FindBy annotations
+        PageFactory.initElements(driver, this);
+
+    }
+
+    /**
+     * Get current page title
+     * @return String page title
+     */
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    /**
+     * Get current URL
+     * @return String current URL
+     */
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 }
