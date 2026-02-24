a725d4322c32d50f916f17f47f764ec7227b2bc6
Harriet Effah <harriet.effah@amalitech.com>
2026-02-24 15:39:38 +0000

    feat: Implemented CustomerSelectPage for customer login

--- src/main/java/com/xyz/pages/CustomerSelectPage.java
+++ src/main/java/com/xyz/pages/CustomerSelectPage.java
@@ -1,4 +1,101 @@
 package com.xyz.pages;
 
-public class CustomerSelectPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Page Object for Customer selection page.
+// * Allows customers to select their name from dropdown.
+
+public class CustomerSelectPage extends BasePage {
+
+    // Customer dropdown
+    @FindBy(id = "userSelect")
+    private WebElement customerDropdown;
+
+    // Login button
+    @FindBy(className = "btn-default")
+    private WebElement loginBtn;
+
+    public CustomerSelectPage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     * Select customer by visible text
+     * @param customerName full name as "FirstName LastName"
+     */
+    public void selectCustomer(String customerName) {
+
+        wait.until(d -> customerDropdown.isDisplayed());
+        Select select = new Select(customerDropdown);
+        select.selectByVisibleText(customerName);
+    }
+
+    /**
+     * Select customer by index
+     * @param index dropdown index (0 for first customer)
+     */
+    public void selectCustomerByIndex(int index) {
+
+        wait.until(d -> customerDropdown.isDisplayed());
+        Select select = new Select(customerDropdown);
+        select.selectByIndex(index);
+    }
+
+    /**
+     * Click Login button
+     * @return CustomerDashboardPage instance
+     */
+    public CustomerDashboardPage clickLogin() {
+
+        wait.until(d -> loginBtn.isEnabled());
+        loginBtn.click();
+        return new CustomerDashboardPage(driver);
+    }
+
+    /**
+     * Complete customer login flow
+     * @param customerName customer name to select
+     * @return CustomerDashboardPage
+     */
+    public CustomerDashboardPage loginAsCustomer(String customerName) {
+
+        selectCustomer(customerName);
+        return clickLogin();
+    }
+
+    /**
+     * Get list of all available customers
+     * @return List<String> of customer names
+     */
+    public List<String> getAllCustomers() {
+        wait.until(d -> customerDropdown.isDisplayed());
+        Select select = new Select(customerDropdown);
+        return select.getOptions().stream()
+                .skip(1) // Skip first option (---Your Name---)
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Check if customer exists in dropdown
+     * @param customerName customer name to check
+     * @return boolean true if exists
+     */
+    public boolean isCustomerAvailable(String customerName) {
+        return getAllCustomers().contains(customerName);
+    }
+
+    /**
+     * Get customer count
+     * @return int number of customers
+     */
+    public int getCustomerCount() {
+        return getAllCustomers().size();
+    }
 }
